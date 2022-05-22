0809ad3840f773a024dc69f39ead67fec8290e2f
sergei-p <sergeidev2494@gmail.com>
2022-05-21 21:05:42 -0700

    Created new Card Component

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import "./css/Card.css"
+
+function Card() {
+  return(
+    <div className="card">
+    <img className="cardImg" src="https://picsum.photos/200/300" alt="Image" />
+    <div className="cardContainer">
+      <h2><b>PokemonName</b></h2>
+      <p>ability</p>
+      <button class="cardButton">More Info</button>
+    </div>
+  </div>
+  )
+}
+
+export default Card;
